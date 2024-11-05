6970b2d4c3ebf9cfc9a68bdcb8b0e90c9b110f8f
fazziclay <fazziclay@gmail.com>
2024-11-05 23:30:29 +0300

    feat(notes): added lock support, specKeys for optimize traffic

--- src/main/java/com/fazziclay/fclaybackend/notes/NoteDto.java
+++ src/main/java/com/fazziclay/fclaybackend/notes/NoteDto.java
@@ -1,21 +1,29 @@
 package com.fazziclay.fclaybackend.notes;
 
 import com.fazziclay.fclaybackend.Logger;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import org.jetbrains.annotations.NotNull;
+import lombok.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 @Setter
 @Getter
 @Builder
 @AllArgsConstructor
 public class NoteDto {
-    @NotNull private String text;
+    private static final HashMap<String, Consumer<NoteDto>> KEYS_AND_CLEANERS = new HashMap<>();
+
+    static {
+        KEYS_AND_CLEANERS.put("text", noteDto -> noteDto.setText(null));
+        KEYS_AND_CLEANERS.put("latestEdit", noteDto -> noteDto.setLatestEdit(null));
+        KEYS_AND_CLEANERS.put("l", noteDto -> noteDto.setL(null));
+    }
+
+    private String text;
     private Long latestEdit;
+    private Integer l;
 
     public void validate() {
         Objects.requireNonNull(text);
@@ -33,5 +41,12 @@ public class NoteDto {
     public void overwriteFrom(NoteDto noteDto) {
         this.text = noteDto.text;
         this.latestEdit = noteDto.latestEdit;
+        // here need editBy? it used only for response
+    }
+
+    public void clearExclude(String[] specKeys) {
+        val keysToRemove = KEYS_AND_CLEANERS.keySet();
+        List.of(specKeys).forEach(keysToRemove::remove);
+        keysToRemove.forEach(s -> KEYS_AND_CLEANERS.get(s).accept(this));
     }
 }
--- src/main/java/com/fazziclay/fclaybackend/notes/NotesController.java
+++ src/main/java/com/fazziclay/fclaybackend/notes/NotesController.java
@@ -3,6 +3,7 @@ package com.fazziclay.fclaybackend.notes;
 import com.fazziclay.fclaybackend.Util;
 import com.fazziclay.fclaybackend.notes.service.NotesService;
 import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +17,27 @@ public class NotesController {
     private NotesService notesService;
 
     @GetMapping
-    public ResponseEntity<?> getNoteText(@RequestHeader(value = "Authorization", required = false) String authorization) {
-        return handle(() -> notesService.getNote(authorization));
+    public ResponseEntity<?> getNoteText(@RequestHeader(value = "Authorization", required = false) String authorization, @Nullable @RequestParam(value = "specKeys", required = false) String specKeysParam) {
+        return handle(() -> {
+            String[] specKeys = null;
+            if (specKeysParam != null) {
+                specKeys = specKeysParam.split(",");
+            }
+            return notesService.getNote(authorization, specKeys);
+        });
     }
 
     @PatchMapping
     public ResponseEntity<?> setNoteText(@RequestHeader(value = "Authorization", required = false) String authorization, @RequestBody NoteDto note) {
         return handle(() -> notesService.setNote(authorization, note));
+    }
+
+    @PostMapping("/lock")
+    public ResponseEntity<?> lockNote(@RequestHeader(value = "Authorization", required = false) String authorization) {
+        return Util.handleError(() -> {
+            notesService.lockNote(authorization);
+            return "";
+        }, HttpStatus.NO_CONTENT);
     }
 
     private <T> ResponseEntity<?> handle(Supplier<T> supplier) {
